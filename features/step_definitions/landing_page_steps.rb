0f9a22217ca33bcf9ba96b5b06eb907288a68b1e
Francesco <francesco.falchi@gmail.com>
2017-11-27 18:39:16 +0100

    Create new feature create_articles, defining steps

--- features/step_definitions/landing_page_steps.rb
+++ features/step_definitions/landing_page_steps.rb
@@ -11,3 +11,23 @@ Given("the following articles exists") do |table|
     Article.create!(hash)
   end
 end
+
+Given("I visit the landing page") do
+  visit root_path
+end
+
+When("I click {string} link") do |new_article|
+  click_link_or_button new_article
+end
+
+When("I fill in {string} with {string}") do |string, string2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I click {string} button") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should be on {string} page") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
