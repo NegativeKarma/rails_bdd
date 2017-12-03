3f3476e93ba66f9f461dfffad456ba24b34c519c
Francesco <francesco.falchi@gmail.com>
2017-12-03 14:47:22 +0100

    Add validations, unit tests all green

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'DB table' do
+    it { is_expected.to have_db_column :id }
+    it { is_expected.to have_db_column :reader }
+    it { is_expected.to have_db_column :content }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :reader }
+    it { is_expected.to validate_presence_of :content }
+  end
+
+  describe 'Factory' do
+    it 'should be valid' do
+      expect(FactoryBot.create(:article)).to be_valid
+    end
+  end
 end
